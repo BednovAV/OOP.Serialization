3120a3f1b44e4cb6e55dce20306a1e84e03476ef
Andrey <bednov_av@mail.ru>
2020-11-23 13:52:52 +0400

    Добавьте файлы проекта.

---
+++ OOP.Serialization/Serialazer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OOP.Serialization
+{
+    public static class Serialazer
+    {
+        public static void BinSer<T>(string file, T item)
+        {
+            BinaryFormatter serializer = new BinaryFormatter();
+            using (FileStream f = new FileStream(file + ".dat", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(f, item);
+                Console.WriteLine($"{file + ".dat"} сериализован");
+            }
+        }
+
+        public static void BinDes<T>(string file, ref T item)
+        {
+            BinaryFormatter serializer = new BinaryFormatter();
+            using (FileStream f = File.OpenRead(file + ".dat"))
+            {
+                item = (T)serializer.Deserialize(f);
+                Console.WriteLine($"{file + ".dat"} десериализован");
+            }
+        }
+
+        public static void XmlSer<T>(string file, T item)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (FileStream f = new FileStream(file + ".xml", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(f, item);
+                Console.WriteLine($"{file + ".dat"} сериализован");
+            }
+        }
+    }
+}
